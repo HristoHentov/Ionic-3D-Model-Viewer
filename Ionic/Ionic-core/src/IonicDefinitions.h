e70b69fadf4b5b694264bc704c91c048cf786699
Hentov <h_hentov@yahoo.com>
2016-11-28 20:09:56 +0200

    Formatted Defines

--- Ionic/Ionic-core/src/IonicDefinitions.h
+++ Ionic/Ionic-core/src/IonicDefinitions.h
@@ -1,10 +1,10 @@
 #pragma once
 
 /* Window Params*/
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
+#define WINDOW_WIDTH		1280
+#define WINDOW_HEIGHT		720
 
-#define WINDOW_TITLE "Ionic 3D Model Viewer"
+#define WINDOW_TITLE		"Ionic 3D Model Viewer"
 /* END */
 
 /* Conosole Colors */
@@ -12,12 +12,18 @@
 #define TEXT_COLOR_RED		12
 #define TEXT_COLOR_YELLOW	14
 #define TEXT_COLOR_WHITE	15
-/* END*/
+/* END */
 
 /* Error Messages*/
 #define GLFW_INIT_FAIL		"Error: GLFW Initialization failed."
 #define GLEW_INIT_FAIL		"Error: GLEW Initialization failed."
 #define INIT_SUCCSESS		"Initialization successfull."
 #define OPENGL_VERSION		"OpenGL Version: "
+/* END */
+
+/* General Constants */
+#define TOTAL_KEYS			1024
+#define TOTAL_BUTTONS		32
+/* END */
 
 
